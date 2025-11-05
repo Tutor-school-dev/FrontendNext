af8fb932da0de3bcc9d85ebb27a4dc4912679e0a
Gaurav Bhinda <tutorschooldev@gmail.com>
2025-11-05 20:30:30 +0530

    Redirecting /apply to /auth

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/apply',
+        destination: '/auth?model=teacher',
+        permanent: true,
+      },
+    ];
+  },
   experimental: {
     optimizePackageImports: ['lucide-react'],
   },
